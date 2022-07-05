71971b1e751213dabb60fae90bae1fbe16b82e09
delphox60 <delphox60@gmail.com>
2022-07-05 10:34:17 +0900

    feat(server): create server Spring-Boot project

---
+++ src/main/java/day6/fullbang/FullbangApplication.java
@@ -0,0 +1,13 @@
+package day6.fullbang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullbangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullbangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/day6/fullbang/FullbangApplicationTests.java
@@ -0,0 +1,13 @@
+package day6.fullbang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullbangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
