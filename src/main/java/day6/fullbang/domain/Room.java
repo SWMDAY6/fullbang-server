1eb43cec701a4d55e2673897cd737a006d37f9ba
delphox60 <delphox60@gmail.com>
2022-07-20 22:58:59 +0900

    feat(domain): define Room class

--- src/main/java/day6/fullbang/domain/Room.java
+++ src/main/java/day6/fullbang/domain/Room.java
@@ -1,4 +1,44 @@
 package day6.fullbang.domain;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "room")
 public class Room {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "room_id")
+    private int id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "place_id")
+    private Place place;
+
+    private String name;
+
+    @OneToMany(mappedBy = "room", cascade = CascadeType.ALL)
+    private List<Image> images;
+
+    private int standardCapacity;
+    private int maximumCapacity;
+
+    private String useInfo;
+
+    @OneToMany(mappedBy = "room", cascade = CascadeType.ALL)
+    private List<Product> products;
+
+    private LocalDateTime crawledAt;
 }
