34d2b262246ae4a9e5312a84720a9f87db3c8787
delphox60 <delphox60@gmail.com>
2022-07-20 22:59:24 +0900

    feat(domain): define Image class

--- src/main/java/day6/fullbang/domain/Image.java
+++ src/main/java/day6/fullbang/domain/Image.java
@@ -1,4 +1,30 @@
 package day6.fullbang.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "image")
 public class Image {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "image_id")
+    private int id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "place_id")
+    private Place place;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "place_id")
+    private Room room;
+
+    private String url;
 }
