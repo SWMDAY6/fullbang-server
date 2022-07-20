f8c58b1128c1f3c494a6837217ce25cd2ab4405e
delphox60 <delphox60@gmail.com>
2022-07-20 22:58:04 +0900

    feat(domain): define Place class - +) define PlaceType enum

--- src/main/java/day6/fullbang/domain/Place.java
+++ src/main/java/day6/fullbang/domain/Place.java
@@ -1,4 +1,52 @@
 package day6.fullbang.domain;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "place")
+@NoArgsConstructor
 public class Place {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "place_id")
+    private Long id;
+
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    private PlaceType placeType;
+
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "address_id")
+    private Address address;
+
+    private String contactInfo;
+
+    private String detailInfo;
+
+    private Boolean smokingAvailability;
+
+    private LocalDateTime crawledAt;
+
+    @OneToMany(mappedBy = "place", cascade = CascadeType.ALL)
+    private final List<Room> rooms = new ArrayList<>();
 }
--- src/main/java/day6/fullbang/domain/PlaceType.java
+++ src/main/java/day6/fullbang/domain/PlaceType.java
@@ -1,2 +1,5 @@
-package day6.fullbang.domain;public enum PlaceType {
+package day6.fullbang.domain;
+
+public enum PlaceType {
+    MOTEL, HOTEL_RESORT, PENSION, GUESTHOUSE, CAMPING, ETC
 }
