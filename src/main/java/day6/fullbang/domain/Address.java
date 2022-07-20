c651f97451d6bf5bc0e95e7f98383eaff70bebc9
delphox60 <delphox60@gmail.com>
2022-07-20 22:59:12 +0900

    feat(domain): define Address class

--- src/main/java/day6/fullbang/domain/Address.java
+++ src/main/java/day6/fullbang/domain/Address.java
@@ -1,4 +1,29 @@
 package day6.fullbang.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "address")
 public class Address {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "address_id")
+    private int id;
+
+    @OneToOne(mappedBy = "address", fetch = FetchType.LAZY)
+    private Place place;
+
+    private String siDo;
+    private String siGunGu;
+    private String eupMyeonDong;
+    private String addressLocality;
+    private String streetAddress;
+
 }
