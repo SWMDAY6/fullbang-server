9941dfa95befa72cd67155374778598e4e60b9c5
delphox60 <delphox60@gmail.com>
2022-07-27 16:09:19 +0900

    fix(controller): load id parameter by path variable

--- src/main/java/day6/fullbang/controller/MarketPriceController.java
+++ src/main/java/day6/fullbang/controller/MarketPriceController.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -19,7 +20,7 @@ public class MarketPriceController {
 
     @GetMapping("/price/{id}")
     @ResponseBody
-    public PriceDto getPriceByProductId(@RequestParam(name = "id") Long id) {
+    public PriceDto getPriceByProductId(@PathVariable(name = "id") Long id) {
 
         Product product = productService.findOne(id);
 
