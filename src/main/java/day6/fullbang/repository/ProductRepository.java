ff031d2f6b115ad8c10a5b23b8e670ad93f19b69
delphox60 <delphox60@gmail.com>
2022-07-27 09:47:09 +0900

    feat(repository): find products by place

--- src/main/java/day6/fullbang/repository/ProductRepository.java
+++ src/main/java/day6/fullbang/repository/ProductRepository.java
@@ -1,9 +1,12 @@
 package day6.fullbang.repository;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 
 import org.springframework.stereotype.Repository;
 
+import day6.fullbang.domain.Place;
 import day6.fullbang.domain.Product;
 import lombok.RequiredArgsConstructor;
 
@@ -15,5 +18,11 @@ public class ProductRepository {
 
     public Product findOne(Long id) {
         return em.find(Product.class, id);
+    }
+
+    public List<Product> searchProductByPlace(Place place) {
+        return em.createQuery("select p from Product p where p.room.place = :place", Product.class)
+            .setParameter("place", place)
+            .getResultList();
     }
 }
