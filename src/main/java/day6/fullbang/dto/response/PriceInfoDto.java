18dcd7de3832e98e8ebeec33787cb73b83f4a9a2
delphox60 <delphox60@gmail.com>
2022-07-29 17:08:54 +0900

    fix(dto): add getter method

--- src/main/java/day6/fullbang/dto/response/PriceInfoDto.java
+++ src/main/java/day6/fullbang/dto/response/PriceInfoDto.java
@@ -2,25 +2,14 @@ package day6.fullbang.dto.response;
 
 import java.time.LocalDate;
 
-import com.google.gson.JsonObject;
-
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+@Getter
 @AllArgsConstructor
 public class PriceInfoDto {
     private final String placeName;
     private final String roomName;
     private final Long price;
     private final LocalDate date;
-
-    public String getJson() {
-        JsonObject jsonObject = new JsonObject();
-
-        jsonObject.addProperty("place_name", placeName);
-        jsonObject.addProperty("room_name", roomName);
-        jsonObject.addProperty("price", price);
-        jsonObject.addProperty("date", String.valueOf(date));
-
-        return jsonObject.toString();
-    }
 }
