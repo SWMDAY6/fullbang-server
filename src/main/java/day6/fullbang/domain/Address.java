4165ba760b5e67e51e0702d6102485a42fa99d04
kimjyun <jhkim311@naver.com>
2022-08-04 18:37:50 +0900

    feat : 좌표값 내 숙박업체 조회 API 구현

--- src/main/java/day6/fullbang/domain/Address.java
+++ src/main/java/day6/fullbang/domain/Address.java
@@ -3,23 +3,26 @@ package day6.fullbang.domain;
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
 
+import lombok.Getter;
+
 @Embeddable
+@Getter
 public class Address {
 
-    private String addressFullName;
+	private String addressFullName;
 
-    @Column(name = "region_1depth_name")
-    private String region1DepthName;
+	@Column(name = "region_1depth_name")
+	private String region1DepthName;
 
-    @Column(name = "region_2depth_name")
-    private String region2DepthName;
+	@Column(name = "region_2depth_name")
+	private String region2DepthName;
 
-    @Column(name = "region_3depth_name")
-    private String region3DepthName;
+	@Column(name = "region_3depth_name")
+	private String region3DepthName;
 
-    private String addressCode;
+	private String addressCode;
 
-    private double latitude;
+	private double latitude;
 
-    private double longitude;
+	private double longitude;
 }
---
+++ src/main/java/day6/fullbang/dto/CoordinateDto.java
@@ -0,0 +1,16 @@
+package day6.fullbang.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CoordinateDto {
+	private double longitudeStart;
+	private double latitudeStart;
+
+	private double longitudeEnd;
+	private double latitudeEnd;
+}
