581a84e11f021cbea1ba71fabd93f1eeae0ddc7b
delphox60 <delphox60@gmail.com>
2022-08-05 10:02:21 +0900

    feat(service): 조건에 맞는 priceInfoDto를 가져오는 로직 구현

---
+++ src/main/java/day6/fullbang/service/ProductService.java
@@ -0,0 +1,22 @@
+package day6.fullbang.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import day6.fullbang.domain.Product;
+import day6.fullbang.dto.product.PriceInfoDto;
+import day6.fullbang.dto.service.MarketPriceCondition;
+import day6.fullbang.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public List<PriceInfoDto> getPriceInfoByAddressCode(MarketPriceCondition marketPriceCondition) {
+        return productRepository.getPriceInfoByAddressCode(marketPriceCondition);
+    }
+}
