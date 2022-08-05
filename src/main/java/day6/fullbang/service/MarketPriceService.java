a66aec6e47009be7e391d5b8a38874a397a5c992
delphox60 <delphox60@gmail.com>
2022-08-05 09:54:35 +0900

    feat(service): 시세 산출 비즈니스 로직 구현

---
+++ src/main/java/day6/fullbang/service/MarketPriceService.java
@@ -0,0 +1,33 @@
+package day6.fullbang.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import day6.fullbang.dto.product.PriceInfoDto;
+import day6.fullbang.dto.response.MarketPriceDto;
+import day6.fullbang.dto.service.MarketPriceCondition;
+import day6.fullbang.util.MarketPriceCalculator;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class MarketPriceService {
+
+    private final ProductService productService;
+
+    public MarketPriceDto getByAddressCode(MarketPriceCondition marketPriceCondition) {
+
+        List<PriceInfoDto> targetProducts = productService.getPriceInfoByAddressCode(marketPriceCondition);
+
+        List<Long> prices = new ArrayList<>();
+        targetProducts.forEach(priceInfo -> prices.add(priceInfo.getPrice()));
+
+        Double mean = MarketPriceCalculator.getMean(prices);
+        Double confidenceIntervalOffset = MarketPriceCalculator.getConfidenceIntervalOffset(prices);
+
+        return new MarketPriceDto(mean, mean - confidenceIntervalOffset, mean + confidenceIntervalOffset);
+    }
+
+}
