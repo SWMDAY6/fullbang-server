25461196a2ae07ce7b4db3d45ab95174d803e121
KimJyun <jhkim311@naver.com>
2022-08-08 13:56:37 +0900

    [DAY6-147] refactor : React에서 GET Method 에서 RequestBody를 받지 못하는 이슈로 POST Method로 수정한 것을 다시 GET Method로 변경하고, RequestParam 으로 변경

--- src/main/java/day6/fullbang/controller/PlaceController.java
+++ src/main/java/day6/fullbang/controller/PlaceController.java
@@ -1,14 +1,17 @@
 package day6.fullbang.controller;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import day6.fullbang.domain.Place;
+import day6.fullbang.domain.PlaceType;
 import day6.fullbang.dto.CoordinateRangeDto;
 import day6.fullbang.dto.request.FilterOptionRequestDto;
 import day6.fullbang.dto.response.PlaceResponseDto;
@@ -21,8 +24,15 @@ public class PlaceController {
 
     private final PlaceService placeService;
 
-    @PostMapping("/places")
-    public List<PlaceResponseDto> readAllPlaces(@RequestBody CoordinateRangeDto coordinateRangeDto) {
+    @GetMapping("/places")
+    public List<PlaceResponseDto> readAllPlaces(
+            @RequestParam("longitudeStart") Double longitudeStart,
+            @RequestParam("latitudeStart") Double latitudeStart,
+            @RequestParam("longitudeEnd") Double longitudeEnd,
+            @RequestParam("latitudeEnd") Double latitudeEnd) {
+        CoordinateRangeDto coordinateRangeDto = new CoordinateRangeDto(longitudeStart, latitudeStart, longitudeEnd,
+                latitudeEnd);
+
         List<Place> placesByCoordinate = placeService.findPlacesByCoordinate(coordinateRangeDto);
 
         List<PlaceResponseDto> responsePlaces = new ArrayList<>();
@@ -35,8 +45,15 @@ public class PlaceController {
 
     }
 
-    @PostMapping("/places/option")
-    public List<PlaceResponseDto> readFilteredPlaces(@RequestBody FilterOptionRequestDto filterOptionRequestDto) {
+    @GetMapping("/places/option")
+    public List<PlaceResponseDto> readFilteredPlaces(
+            @RequestParam("parkingAvailability") Boolean parkingAvailability,
+            @RequestParam("placeType") PlaceType placeType,
+            @RequestParam("maximumCapacity") Integer maximumCapacity,
+            @RequestParam("checkInDateTime") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate checkInDateTime) {
+        FilterOptionRequestDto filterOptionRequestDto =
+                new FilterOptionRequestDto(parkingAvailability, placeType, maximumCapacity, checkInDateTime);
+
         List<Place> filteredPlaces = placeService.findPlacesByOption(filterOptionRequestDto);
         List<PlaceResponseDto> filteredResponsePlaces = filteredPlaces.stream()
                 .map(p -> new PlaceResponseDto(p))
