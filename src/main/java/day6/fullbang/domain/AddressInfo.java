63fee1bfed462579e6ee3ae62ab85a67b9fe1571
delphox60 <delphox60@gmail.com>
2022-08-10 00:05:56 +0900

    feat(domain): addressInfo 주소 코드 패턴 로직 구현 및 패키지 이동

--- src/main/java/day6/fullbang/domain/AddressInfo.java
+++ src/main/java/day6/fullbang/domain/AddressInfo.java
@@ -1,4 +1,4 @@
-package day6.fullbang.dto.address;
+package day6.fullbang.domain;
 
 import lombok.AllArgsConstructor;
 
@@ -13,4 +13,23 @@ public class AddressInfo {
     private final String region3DepthAddressCode;
     private final Double latitude;
     private final Double longitude;
+
+    public String getAddressCodeHead(int regionDepth) {
+
+        String result = region1DepthAddressCode;
+
+        if (regionDepth == 1) {
+            return result;
+        }
+
+        result += region2DepthAddressCode;
+
+        if (regionDepth == 2) {
+            return result;
+        }
+
+        result += region3DepthAddressCode;
+
+        return result;
+    }
 }
