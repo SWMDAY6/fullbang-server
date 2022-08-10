eec8d8bd7304a23e605f90da1fbc5ee48a812a4f
delphox60 <delphox60@gmail.com>
2022-08-10 10:36:06 +0900

    fix(controller): 시세 조회 GET 요청 포맷 수정
    - request body -> request parameter

--- src/main/java/day6/fullbang/controller/ProductController.java
+++ src/main/java/day6/fullbang/controller/ProductController.java
@@ -5,8 +5,10 @@ import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import day6.fullbang.domain.PlaceType;
 import day6.fullbang.dto.request.CoordinateRangeDto;
 import day6.fullbang.dto.request.MarketPriceConditionDto;
 import day6.fullbang.dto.response.MarketPriceDto;
@@ -28,9 +30,19 @@ public class ProductController {
 
     @GetMapping("/product/inRange/marketPrice/{regionDepth}")
     public List<MarketPriceDto> getMarketPriceByCoordinateRange(
-        @RequestBody MarketPriceConditionDto marketPriceConditionDto,
-        @PathVariable CoordinateRangeDto coordinateRangeDto,
+        @RequestParam PlaceType placeType, @RequestParam String date,
+        @RequestParam Integer capacity, @RequestParam Boolean parkingAvailability,
+        @RequestParam Double latitudeStart,
+        @RequestParam Double latitudeEnd,
+        @RequestParam Double longitudeStart,
+        @RequestParam Double longitudeEnd,
         @PathVariable int regionDepth) {
+
+        MarketPriceConditionDto marketPriceConditionDto = new MarketPriceConditionDto(placeType, date, capacity,
+            parkingAvailability);
+
+        CoordinateRangeDto coordinateRangeDto = new CoordinateRangeDto(latitudeStart, latitudeEnd, longitudeStart,
+            longitudeEnd);
 
         return marketPriceService.getByCoordinateRange(marketPriceConditionDto, coordinateRangeDto, regionDepth);
     }
