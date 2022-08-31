6dee542c7ce7aeef54b8f463f1e01f8fa7532b7d
delphox60 <delphox60@gmail.com>
2022-08-31 19:38:02 +0900

    feat: 시세 정보 DTO 내 숙소 유형 정보를 저장하는 멤버 변수 추가

--- src/main/java/day6/fullbang/dto/response/MarketPriceDto.java
+++ src/main/java/day6/fullbang/dto/response/MarketPriceDto.java
@@ -1,5 +1,6 @@
 package day6.fullbang.dto.response;
 
+import day6.fullbang.domain.PlaceType;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -10,5 +11,6 @@ public class MarketPriceDto {
     private final Double mean;
     private final Double minMeanOfRange;
     private final Double maxMeanOfRange;
+    private final PlaceType placeType;
 
 }
