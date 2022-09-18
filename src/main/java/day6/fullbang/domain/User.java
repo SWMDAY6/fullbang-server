ea2fdde0e30b3e83ec766c8cd8b8485150f5c683
delphox60 <delphox60@gmail.com>
2022-09-18 12:32:17 +0900

    feat: user 엔티티 클래스 구현

---
+++ src/main/java/day6/fullbang/domain/User.java
@@ -0,0 +1,31 @@
+package day6.fullbang.domain;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @Column(name = "user_id", unique = true)
+    private String id;
+
+    @Column(name = "user_name", unique = true)
+    private String name;
+
+    @Column
+    private String password;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+//    TODO implement AUTH
+}
