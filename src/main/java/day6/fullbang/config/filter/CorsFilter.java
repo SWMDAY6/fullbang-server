229b20addd864ba71f4ce2d771497bb33318b290
JiHyun <31675698+KimJyun@users.noreply.github.com>
2022-09-27 09:23:24 +0900

    [DAY6-186] fix : Cors error 해결 (#21)

--- src/main/java/day6/fullbang/config/filter/CorsFilter.java
+++ src/main/java/day6/fullbang/config/filter/CorsFilter.java
@@ -1,6 +1,8 @@
 package day6.fullbang.config.filter;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -30,13 +32,17 @@ public class CorsFilter implements Filter {
         HttpServletRequest request = (HttpServletRequest)req;
         HttpServletResponse response = (HttpServletResponse)res;
 
-        response.setHeader("Access-Control-Allow-Origin",
-            "http://localhost:3000"); // TODO modify
-        response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("Access-Control-Allow-Methods", "*");
-        response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers",
-            "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+        List<String> allowOrigin = Arrays.asList("http://localhost:3000", "http://www.fullbang.kr",
+            "http://fullbang.kr");
+
+        if (allowOrigin.contains(request.getHeader("Origin"))) {
+            response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+            response.setHeader("Access-Control-Allow-Credentials", "true");
+            response.setHeader("Access-Control-Allow-Methods", "*");
+            response.setHeader("Access-Control-Max-Age", "3600");
+            response.setHeader("Access-Control-Allow-Headers",
+                "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+        }
 
         if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
             response.setStatus(HttpServletResponse.SC_OK);
