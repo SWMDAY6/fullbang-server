361adefa2d3a6406e3a77e6653a2536a98369535
delphox60 <delphox60@gmail.com>
2022-11-13 12:25:45 +0900

    feat: marketprice 엔티티 구현

---
+++ src/main/java/day6/fullbang/domain/MarketPrice.java
@@ -0,0 +1,36 @@
+package day6.fullbang.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "market_price")
+@NoArgsConstructor
+@Getter
+public class MarketPrice {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "market_price_id")
+    private Long id;
+
+    @Column(name = "place_type")
+    @Enumerated(EnumType.STRING)
+    private PlaceType type;
+
+    private String AddressCodeHead;
+}
