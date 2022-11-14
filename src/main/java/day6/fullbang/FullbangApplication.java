026d690ff4a71759ee132581493041b157abe146
KimJyun <jhkim311@naver.com>
2022-11-14 12:04:38 +0900

    feat : AI 데이터를 위한 백엔드 API 구현[DAY6-214]

--- src/main/java/day6/fullbang/FullbangApplication.java
+++ src/main/java/day6/fullbang/FullbangApplication.java
@@ -3,9 +3,11 @@ package day6.fullbang;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class FullbangApplication {
 
